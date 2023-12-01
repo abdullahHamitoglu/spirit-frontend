38548f8429c93664c18ef58fb2bc0b1dc449b966
abdulahhamad <alhmada176@gmail.com>
2023-12-01 19:35:25 +0300

    update

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,5 @@ module.exports = {
   devServer: {
     contentBase: "./app",
     compress: true,
-    proxy: {
-      "/graphql": {
-        target: "https://http://localhost:4000/graphql",
-        secure: false,
-      },
-    },
   },
 };
