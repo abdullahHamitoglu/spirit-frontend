fe17b5e10bf1fc7f3121d74b2a002d9b320f21c1
abdulahhamad <alhmada176@gmail.com>
2023-12-15 14:01:05 +0300

    update translate lang chars

--- components/headers/common/currency.js
+++ components/headers/common/currency.js
@@ -16,8 +16,8 @@ const Currency = ({ icon }) => {
   useEffect(() => {
     fetchCurrencies(locale);
   }, []);
-  
-  const handleCurrencyClick = async (currencyID,currencyCode) => {
+
+  const handleCurrencyClick = async (currencyID, currencyCode) => {
     await setCurrency(locale, currencyID);
     await setCookie(null, "currencyCode", currencyCode, {
       maxAge: 7 * 24 * 60 * 60,
@@ -25,7 +25,7 @@ const Currency = ({ icon }) => {
     });
     router.reload();
   };
-  
+
   return (
     <li className="onhover-div mobile-setting">
       <div>
@@ -35,22 +35,22 @@ const Currency = ({ icon }) => {
       <div className="show-div setting">
         <h6>{t('language')}</h6>
         <ul>
-            <li>
-              <Link href={{ pathname: router.pathname, query: router.query, }} locale="ar">
-                {t('ar-kw')}
-              </Link>
-            </li>
-            <li>
-              <Link href={{ pathname: router.pathname, query: router.query, }} locale="en">
-                {t('en-us')}
-              </Link>
-            </li>
+          <li>
+            <Link href={{ pathname: router.pathname, query: router.query, }} locale="ar">
+              {t("arabic")}
+            </Link>
+          </li>
+          <li>
+            <Link href={{ pathname: router.pathname, query: router.query, }} locale="en">
+              {t("english")}
+            </Link>
+          </li>
         </ul>
         <h6>{t("currency")}</h6>
         <ul className="list-inline">
           {currencies.data && currencies.data.map((currency, i) => (
             <li className={selectedCurrency.code.toLowerCase() == currency.code.toLowerCase() ? 'active fw-bolder' : ''} key={i} title={currency.name}>
-              <div onClick={() => handleCurrencyClick(currency.id , currency.code)} title={currency.name}>
+              <div onClick={() => handleCurrencyClick(currency.id, currency.code)} title={currency.name}>
                 {currency.code} {currency.symbol}
               </div>
             </li>
