dff4fccccdf5effd4247ecd6d42410d4111c6d9b
abdulahhamad <alhmada176@gmail.com>
2023-12-27 02:41:06 +0300

    fix loading in filter

--- pages/_app.js
+++ pages/_app.js
@@ -29,28 +29,35 @@ function MyApp({ Component, pageProps }) {
     const { locale } = useRouter();
     const { token, registerDevice } = useUserStore();
     const { t } = useTranslation();
-
+    console.log(url, router.asPath);
     const [loading, setLoading] = useState(false);
     useEffect(() => {
-        if (router.asPath.includes('products')) {
 
-            const handleStart = (url) => (url !== router.asPath) && setLoading(true);
-            const handleComplete = (url) => (url === router.asPath) && setLoading(false);
-
-            router.events.on('routeChangeStart', handleStart)
-            router.events.on('routeChangeComplete', handleComplete)
-            router.events.on('routeChangeError', handleComplete)
-
-            return () => {
-                router.events.off('routeChangeStart', handleStart)
-                router.events.off('routeChangeComplete', handleComplete)
-                router.events.off('routeChangeError', handleComplete)
-                setTimeout(() => {
-                    setLoading(false)
-                }, 10000);
+        const handleStart = (url) => {
+            if (url !== router.asPath && !url.includes("?")) {
+                setLoading(true);
             }
+        };
+
+        const handleComplete = (url) => {
+            if (url === router.asPath && !url.includes("?")) {
+                setLoading(false);
+            }
+        };
+
+        router.events.on('routeChangeStart', handleStart)
+        router.events.on('routeChangeComplete', handleComplete)
+        router.events.on('routeChangeError', handleComplete)
+
+        return () => {
+            router.events.off('routeChangeStart', handleStart)
+            router.events.off('routeChangeComplete', handleComplete)
+            router.events.off('routeChangeError', handleComplete)
+            setTimeout(() => {
+                setLoading(false)
+            }, 10000);
         }
-    })
+    },[router.asPath])
 
     useEffect(() => {
         setIsLoading(true);
