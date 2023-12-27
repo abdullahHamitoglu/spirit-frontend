82a575f18906f23b4b8df50657e05c6d9844f49d
abdulahhamad <alhmada176@gmail.com>
2023-12-27 15:59:26 +0300

    edit gtm

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        {(process.env.NODE_ENV === 'production') &&
+        {/* {(process.env.NODE_ENV === 'production') && */}
           <script dangerouslySetInnerHTML={{
             __html: `(function (w, d, s, l, i) {
                     w[l] = w[l] || []; w[l].push({ 'gtm.start': new Date().getTime(), event: 'gtm.js' });
@@ -12,15 +12,15 @@ export default function Document() {
                     j.async = true; j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl; f.parentNode.insertBefore(j, f);
                 })(window, document, 'script', 'dataLayer', 'GTM-KDKQLGCS');`
           }}></script>
-        }
+        {/* } */}
         <script async src="https://www.googletagmanager.com/gtag/js?id=GTM-KDKQLGCS"></script>
       </Head>
       <body>
-        {(process.env.NODE_ENV === 'production') &&
+        {/* {(process.env.NODE_ENV === 'production') && */}
           <noscript dangerouslySetInnerHTML={{
             __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KDKQLGCS" height="0" width="0" style="display:none;visibility:hidden"></iframe>`
           }}></noscript>
-        }
+        {/* } */}
         <Main />
         <NextScript />
       </body>
